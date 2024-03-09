508ece5a85cb1a105c1f6299bfd32abc01d655ed
biglazyet <xjl505302554@outlook.com>
2024-03-09 21:29:20 +0800

    handler exploration

--- maui.exploratoin/handler.exploration/Pages/HandlerAndVirtualView/FooEntryPage.xaml.cs
+++ maui.exploratoin/handler.exploration/Pages/HandlerAndVirtualView/FooEntryPage.xaml.cs
@@ -1,3 +1,7 @@
+using handler.exploration.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
 namespace handler.exploration.Pages;
 
 public partial class FooEntryPage : ContentPage
@@ -6,4 +10,24 @@ public partial class FooEntryPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void FooEntry_HandlerChanged(object sender, EventArgs e)
+    {
+		var fooEntry = sender as FooEntry;
+        (fooEntry!.Handler!.PlatformView as TextBox)!.GotFocus += OnGotFocus;
+    }
+
+    private void FooEntry_HandlerChanging(object sender, HandlerChangingEventArgs e)
+    {
+        if(e.OldHandler != null)
+        {
+            (e.OldHandler.PlatformView as TextBox)!.GotFocus -= OnGotFocus;
+        }
+    }
+
+    void OnGotFocus(object sender, RoutedEventArgs e)
+    {
+        var textBox = sender as TextBox;
+        textBox!.Select(0, textBox.Text.Length - 1);
+    }
 }
